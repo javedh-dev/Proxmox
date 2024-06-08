c814719ac011d4d3dec529b7fa5654730390ef52
Javed Hussain <javedh.dev@gmail.com>
2024-06-08 23:53:52 +0530

    Added username and password for initial setup

--- ct/bookstack.sh
+++ ct/bookstack.sh
@@ -71,4 +71,6 @@ description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${APP} Setup should be reachable by going to the following URL.
-         ${BL}http://${IP}${CL} \n"
+         ${BL}http://${IP}${CL} \n\n
+         ${YW}email${CL} - admin@admin.com\n
+         ${YW}password${CL} - password\n"
